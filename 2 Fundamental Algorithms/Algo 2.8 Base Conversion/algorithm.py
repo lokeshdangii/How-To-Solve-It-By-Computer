f1c35ad84fe567e64ff6b8e8cb5d0d6c0d66fa06
lokeshdangii <lokeshdangi1045@gmail.com>
2023-02-26 12:01:17 +0530

    Number Base Conversion

--- 2 Fundamental Algorithms/Algo 2.8 Base Conversion/algorithm.py
+++ 2 Fundamental Algorithms/Algo 2.8 Base Conversion/algorithm.py
@@ -1,16 +1,10 @@
-dec_num = int(input("Enter a decimal number :"))
-oct_num = 0
-q = dec_num
-count = 1
-while dec_num == 0:
-    rem = dec_num%8
-    print("Remainder is :",rem)
-    oct_num = oct_num * count +rem  
-    print("octal = ",oct_num)
-    q = int(dec_num/8)
-    count = count * 10
-    q = int(dec_num//8)
-
-
-print("octal = ",oct_num)
-print("the octal conversion of {} is {}".format(dec_num,oct_num))
+num = int(input("Enter any Number "))
+temp= num
+i = 1
+sum = 0
+while temp!=0:
+    rem = int(temp%8)
+    sum = sum +i*rem
+    i= i*10
+    temp= temp/8
+print("Octal Number =", sum)
