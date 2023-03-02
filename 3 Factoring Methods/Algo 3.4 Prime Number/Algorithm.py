101286cd48885728a041653fc5f3201ae80aee5f
lokeshdangii <lokeshdangi1045@gmail.com>
2023-03-02 09:41:49 +0530

    from math import *

---
+++ 3 Factoring Methods/Algo 3.4 Prime Number/Algorithm.py
@@ -0,0 +1,17 @@
+# sieve of eratosthenes
+from math import *
+def genprimes(n):
+        primes = [True]*(n+1)
+        primes [0] = False  
+        primes [1] = False
+        for p in range (2, int(sqrt(n))+1):
+            if primes [p] == True:
+                for i in range(p*p, n+1,p):
+                    primes[i] = False
+        for i in range(0, len(primes)):
+            if primes[i] == True:
+                print (i, end=" ")
+          
+
+n = int(input("Enter n : "))
+genprimes(n)
