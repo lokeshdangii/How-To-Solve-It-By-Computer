38b24cad7f0a17eaf5da81bc83a6eaa53a7b67bb
lokeshdangii <lokeshdangi1045@gmail.com>
2023-03-14 22:33:07 +0530

    Max and Min in array

--- Array Techniques/Algo 4.3 Finding the maximum number in a set/4.3.9.py
+++ Array Techniques/Algo 4.3 Finding the maximum number in a set/4.3.9.py
@@ -1,46 +1,30 @@
 # The minimum and maximum in an array of size n can be found using (3/2)n comparisons instead of 2n comparisons by considering the elements in pairs and comparing the larger element of the pair against the current maximum and the smaller element of the pair against the current minimum. Implement this algorithm
-# arr= [12,1,13,11,64,1,875,34,12,33,1,6,9,875,23]
+arr= [12,1,13,11,64,1,875,34,12,33,1,6,9,875,23]
 
-# max = arr[0]
-# min = arr[0]
+max = arr[0]
+min = arr[0]
 
-# for i in range(1,len(arr)):
-#         if arr[i]<min:
-#             min = arr[i]
-#         if arr[i]>max:
-#             max = arr[i]
+for i in range(1,len(arr)):
+        if arr[i]<min:
+            min = arr[i]
+        if arr[i]>max:
+            max = arr[i]
     
-# print("Maximum = ",max)
-# print("Minimum = ",min)
+print("Maximum = ",max)
+print("Minimum = ",min)
 
 
-# -----Answer By ChatGPT-----
+# ------------------------------------------------------------------------
 
 
 def find_min_max(arr):
-    n = len(arr)
+    
     # Initialize min and max variables
-    '''
-    if n % 2 == 0:
-        # If n is even, compare first two elements to set initial min and max
-        if arr[0] > arr[1]:
-            max_val = arr[0]
-            min_val = arr[1]
-        else:
-            max_val = arr[1]
-            min_val = arr[0]
-        i = 2
-    else:
-        # If n is odd, initialize min and max variables to first element
-        max_val = arr[0]
-        min_val = arr[0]
-        i = 1
-        '''
     i = 0
     max_val = arr[0]
     min_val = arr[0]
     # Compare elements in pairs
-    while i < n-1:
+    while i < len(arr)-1:
         if arr[i] > arr[i+1]:
             if arr[i] > max_val:
                 max_val = arr[i]
@@ -52,6 +36,13 @@ def find_min_max(arr):
             if arr[i] < min_val:
                 min_val = arr[i]
         i += 2
+    
+    # Handle the case where there is an odd number of elements
+    if len(arr) % 2 != 0:
+        if arr[-1] < min_val:
+            min_val = arr[-1]
+        elif arr[-1] > max_val:
+            max_val = arr[-1]
 
     return min_val, max_val
 
---
+++ Array Techniques/Algo 4.3 Finding the maximum number in a set/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+arr= [12,1,13,11,64,1,875,34,12,33,1,6,9,875,23]
+
+# max = arr[0]
+# min = arr[0]
+
+# for i in range(1,len(arr)):
+#         if arr[i]<min:
+#             min = arr[i]
+#         if arr[i]>max:
+#             max = arr[i]
+    
+# print("Maximum = ",max)
+# print("Minimum = ",min)
