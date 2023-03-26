55550342fc504edb9c174367d7ee7d9abf2ed40a
lokeshdangii <lokeshdangi1045@gmail.com>
2023-03-26 17:15:23 +0530

    Removal of duplicate element from an array

--- 4 Array Techniques/Algo 4.4 Removal of Duplicates from an Ordered Array/Algorithm.py
+++ 4 Array Techniques/Algo 4.4 Removal of Duplicates from an Ordered Array/Algorithm.py
@@ -1,18 +1,15 @@
 # Removal of Duplicates from an ordered array 
 arr1 = [12,34,343,13,65,126,44,44,21,34,31,24,24,34]
 n = len(arr1)
-print(n)
-j = int()
-i = 1
-while arr1[i-1] == arr1[i] and i<n:
-    i = i+1
-    if arr1[i-1] == arr1[i]:
-        j = i-1
+j = 0
+for i in range(1,len(arr1)):
+    if arr1[i-1] != arr1[i]:
+        arr1[j] =arr1[i-1]
+        j+=1
 
-while i<n:
-    i = i+1
-    if arr1[i-1] == arr1[i]:
-        j = j+1
-        arr1[j] = arr1[i]
-    
-print("The array is : ", arr1)
+# print(arr1)
+for i in range(0,j):
+    print(arr1[i],end=" ")
+
+
+# this logic is not good for the unsorted array....!
