7c04aa79a95493f4b3999cd94525385903eddb92
lokeshdangii <lokeshdangi1045@gmail.com>
2023-03-26 14:26:45 +0530

    Array duplicate removal

--- 4 Array Techniques/Algo 4.4 Removal of Duplicates from an Ordered Array/Algorithm.py
+++ 4 Array Techniques/Algo 4.4 Removal of Duplicates from an Ordered Array/Algorithm.py
@@ -1,7 +1,18 @@
 # Removal of Duplicates from an ordered array 
-arr1 = [12,34,343,13,65,126,44,44,21,34,31,34]
-list1 = []
-arr = []
+arr1 = [12,34,343,13,65,126,44,44,21,34,31,24,24,34]
+n = len(arr1)
+print(n)
+j = int()
+i = 1
+while arr1[i-1] == arr1[i] and i<n:
+    i = i+1
+    if arr1[i-1] == arr1[i]:
+        j = i-1
 
-Remove duplicate from an array;
-
+while i<n:
+    i = i+1
+    if arr1[i-1] == arr1[i]:
+        j = j+1
+        arr1[j] = arr1[i]
+    
+print("The array is : ", arr1)
---
+++ 4 Array Techniques/Algo 4.4 Removal of Duplicates from an Ordered Array/algo.py
@@ -0,0 +1,12 @@
+arr1 = [22,21,74,23,55,98,98,32,25,56,56,42]
+list_arr = [22]
+i = 1
+
+while i<len(arr1):
+    if arr1[i-1]!=arr1[i]:
+        list_arr.append(arr1[i])
+    
+    i = i+1
+
+
+print("Array is :",list_arr)
