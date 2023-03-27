8014f0fe83fc21a883395ebcdec62b9691e22e0e
lokeshdangii <lokeshdangi1045@gmail.com>
2023-03-27 17:59:19 +0530

    Removal Of Duplicates from array

--- 4 Array Techniques/Algo 4.4 Removal of Duplicates from an Ordered Array/4.4.1.py
+++ 4 Array Techniques/Algo 4.4 Removal of Duplicates from an Ordered Array/4.4.1.py
@@ -2,11 +2,11 @@
 
 arr1 = [2,3,4,5,2,4,2,2,2,2,2,4,4,4,4,4,4]
 print(len(arr1))
-for i in range(1,len(arr1)):
-    for j in range(i-1,len(arr1)-1):
-        if arr1[j] == arr1[i]:
-            arr1.remove(arr1[i])
-        if i ==5:
+for i in range(0,len(arr1)):
+    for j in range(i+1,len(arr1)-1):
+        if arr1[i] == arr1[j]:
+            arr1.remove(arr1[j])
+        if i==5:
             break
         
 print("Array after duplicay removal will be : ",arr1)
