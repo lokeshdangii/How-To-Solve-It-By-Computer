e1c169a1b0ce7bd0cbd532db70674185b6120d93
lokeshdangii <lokeshdangi1045@gmail.com>
2023-04-13 22:08:21 +0530

    Sorting by Insertion

--- 5 Merging, Sorting and Searching/Algo 5.4 Sorting by Insertion/Algorithm.py
+++ 5 Merging, Sorting and Searching/Algo 5.4 Sorting by Insertion/Algorithm.py
@@ -1,28 +1,18 @@
+def insertionSort(arr):
+    
+    for i in range(1,len(arr)):
+        x = arr[i]
+        j = i-1
+
+        while j >=0 and arr[j] > x:
+            arr[j+1] = arr[j]
+            j = j-1
+
+        arr[j+1] = x
+
+    print(arr)
+
+
 arr = [20,35,18,8,14,41,3,39]
-n = len(arr)
-min = arr[0]
-p = 0
-x = int()
-j = int()
+insertionSort(arr)
 
-for i in range(1,n):
-    if arr[i]<min:
-        min = arr[i]
-        p = 0
-
-    arr[p] = arr[1]
-    arr[1] = min
-
-
-for i in range(2,n):
-    x = arr[i]
-    j = i
-
-    while x<arr[j-1]:
-        arr[j] = arr[j-1]
-        j = j-1
-
-    arr[j] = x
-
-
-print(arr)
