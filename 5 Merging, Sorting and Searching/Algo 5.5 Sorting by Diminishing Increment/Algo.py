372b44d31a978eb099dea866f66e8946190c6592
lokeshdangii <lokeshdangi1045@gmail.com>
2023-04-14 23:22:51 +0530

    Sorting by Diminishing Increment/ Shell Sort

--- 5 Merging, Sorting and Searching/Algo 5.5 Sorting by Diminishing Increment/Algo.py
+++ 5 Merging, Sorting and Searching/Algo 5.5 Sorting by Diminishing Increment/Algo.py
@@ -1,20 +1,27 @@
-# a = [20,35,18,8,14,41,3,39]
-a = [15,19,20,38,24,41,30,31,12]
-n = len(a)
-gap = n//2
 
-while gap>=1:
-    j = gap
-    for i in range(0,n-gap):
-        if a[i]>a[j]:
-            a[i],a[j] = a[j],a[i]
-        print("for gap = {} and i = {} j is {}".format(gap,i,j))
-        j = j+1
-    gap = gap//2
+def shellSort(a):
+    n = len(a)
+    gap = n//2
+
+    while gap>=1:
+        j = gap
+        for i in range(0,n-gap):
+            if a[i]>a[j]:
+                # print("for gap {} and i = {} j is {} and swap of {} with {}".format(gap,i,j,a[i],a[j]))
+                a[i],a[j] = a[j],a[i]
+            j = j+1
         
-    
-    
-    
+        # print("\n","array after gap {} and swaping is {}".format(gap,a),"\n")
+
+        gap = gap//2
+            
+    print("Array after sorting : ",a)
+
+    # for i in range(0,n-1):
+    #     if a[i]>a[i+1]:
+    #         a[i],a[i+1] = a[i+1],a[i]
+    # print("array after one more swap : ",a)
 
 
-print(a)
+list = [12,34,78,54,2,3]
+shellSort(list)
