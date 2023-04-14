6b1ba8bef69a8bd13b36585f8fcbed9e612ba642
lokeshdangii <lokeshdangi1045@gmail.com>
2023-04-14 23:38:28 +0530

    Sorting by diminishing increment/Shell Sort

--- 5 Merging, Sorting and Searching/Algo 5.5 Sorting by Diminishing Increment/Algortihm.py
+++ 5 Merging, Sorting and Searching/Algo 5.5 Sorting by Diminishing Increment/Algortihm.py
@@ -1,17 +1,22 @@
+# Shell Sort/ Sorting by deminishing increment 
+
+def shellSort(arr):
+    n = len(arr)
+    gap = n//2
+
+    while gap>=1:
+
+        for j in range(gap,n,1):
+            for i in range(j-gap,-1,-gap): 
+                if arr[i+gap]>arr[i]:
+                    break
+                else:
+                    arr[i+gap],arr[i] = arr[i],arr[i+gap]
+        gap = gap//2
+
+    print(arr)
+    return arr
+
 arr = [20,35,18,8,14,41,3,39]
-n = len(arr)
-gap = n//2
-
-while gap>=1:
-    
-    for j in range(gap,n,1):
-        for i in range(0,j,1): 
-            no = i
-            if arr[i]>arr[j]:
-                arr[i],arr[j] = arr[j],arr[i]
-            else:
-                break
-    gap = gap//2
-    
-
-print(arr)
+a = [14,18,19,37,23,40,29,30,11]
+shellSort(a)
