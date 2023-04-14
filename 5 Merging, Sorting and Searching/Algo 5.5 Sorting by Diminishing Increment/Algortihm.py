c6a3d2fca80afd5371d7cd4a03e2f7768e04ee42
lokeshdangii <lokeshdangi1045@gmail.com>
2023-04-14 22:19:06 +0530

    Sorting by Diminishing Increment

--- 5 Merging, Sorting and Searching/Algo 5.5 Sorting by Diminishing Increment/Algortihm.py
+++ 5 Merging, Sorting and Searching/Algo 5.5 Sorting by Diminishing Increment/Algortihm.py
@@ -1,13 +1,15 @@
 arr = [20,35,18,8,14,41,3,39]
 n = len(arr)
-inc = n
-passs = 0
-while inc > 1:
-    inc = inc//2
-    passs = passs+1
-    for i in range(0,inc):
-        if arr[i]>arr[n-inc+i]:
-            arr[i],arr[n-inc] = arr[n-inc],arr[i]
-    print("Array after {} pass and {} distance is {} ".format(passs,inc,arr))
+gap = n
+
+while gap>=1:
+    gap = gap//2
+    for j in range(gap,n,1):
+        for i in range(j-gap,0,1): #i-gap
+            if arr[i]>arr[j]:
+                arr[i],arr[j] = arr[j],arr[i]
+            else:
+                break
+    
 
 print(arr)
