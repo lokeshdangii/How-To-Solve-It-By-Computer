a7ef19f296cac52773e0bc6d9a140e456f47d30f
lokeshdangii <lokeshdangi1045@gmail.com>
2023-04-15 23:50:43 +0530

    Binary Search

--- 5 Merging, Sorting and Searching/Algo 5.7 Binary Search/Algorithm.py
+++ 5 Merging, Sorting and Searching/Algo 5.7 Binary Search/Algorithm.py
@@ -1,23 +1,30 @@
 
-def binarySearch(arr):
+def binarySearch(arr,ele):
     n = len(arr)
-    x = 44
     lower = arr[0]
     upper = arr[n-1]
-    middle = int()
+    
 
-    while arr[middle] == x or lower > upper :
+    while lower < upper :
         middle = (lower+upper)//2
-        # if arr[middle] == x:
-        #     return middle
-        if x>arr[middle]:
-            lower = middle+1
-        else:
-            upper = middle-1
         
-    # return -1
+        # if arr[middle] == ele:
+        #     return middle
+        # elif ele>arr[middle]:
+        #     lower = middle+1
+        # else:
+        #     upper = middle-1
 
-arr = [10,12,20,23,27,30,31,39,42,44,45,49,57,63,70]
-result = binarySearch(arr)
-print(result)
+        if ele > arr[middle]:
+            lower = middle +1 
+            if arr[lower] == ele:
+                return lower
+        else:
+            upper = middle
+        
+    return -1
+
+arr = [10,12,20,23,27,30,31,39,42,44,45,49,57,63]
+ele = 42
+print(binarySearch(arr,ele))
 
