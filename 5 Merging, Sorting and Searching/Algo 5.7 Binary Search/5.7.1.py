95ed2b520b53814095f056b1293a617ce34a4676
lokeshdangii <lokeshdangi1045@gmail.com>
2023-04-16 23:02:12 +0530

    Binary Search

---
+++ 5 Merging, Sorting and Searching/Algo 5.7 Binary Search/5.7.1.py
@@ -0,0 +1,20 @@
+def binarySearch(arr,ele):
+    lower = 0
+    upper = len(arr)-1
+    
+
+    while lower < upper+1 :
+        middle = (lower+upper)//2
+        
+        if arr[middle] == ele:
+            return middle
+        elif ele>arr[middle]:
+            lower = middle+1
+        else:
+            upper = middle-1
+        
+    return -1
+
+arr = [10,12,20,23,27,30,31,39,42,44,45,49,57,63]
+ele = 42
+print(binarySearch(arr,ele))
